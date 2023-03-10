5946184cd5381e7d93eb315b245c71b2a762ea0d
onishitaisei <taisei.developer@gmail.com>
2023-03-11 03:16:19 +0900

    🚀 add Device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:trip_tonic/firebase_options_dev.dart' as dev;
 import 'package:trip_tonic/firebase_options_prod.dart' as prod;
@@ -29,7 +31,12 @@ Future<void> main() async {
     print('Signed in: uid = $uid');
   }
 
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -37,6 +44,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: MyAppa(),
+    );
+  }
+}
+
+class MyAppa extends StatelessWidget {
+  const MyAppa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('テスト'),
+      ),
+    );
   }
 }
