da8beca3f0c66310a6af448d16006aa7d75c43c6
onishitaisei <taisei.developer@gmail.com>
2023-03-18 19:50:03 +0900

    add

---
+++ lib/presentation/history/history_page.dart
@@ -0,0 +1,52 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:trip_tonic/extensions/context_extension.dart';
+import 'package:trip_tonic/model/api_service.dart';
+
+class HistoryPage extends HookConsumerWidget {
+  const HistoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final text = useState<String>('');
+    final isLoading = useState<bool>(false);
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              TextFormField(),
+              ElevatedButton(
+                onPressed: () async {
+                  isLoading.value = true;
+                  final result = await ApiService(Dio()).createPost('こんにちは');
+                  text.value = result.choices![0].message!.content!;
+                  isLoading.value = false;
+                },
+                child: const Text('リクエスト'),
+              ),
+              Text(text.value),
+              Visibility(
+                visible: isLoading.value,
+                child: Expanded(
+                  child: ColoredBox(
+                    color: Colors.black26,
+                    child: Center(
+                      child: LoadingAnimationWidget.fourRotatingDots(
+                        color: context.theme.primary,
+                        size: 70,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/notification/notification_detail_page.dart
+++ lib/presentation/notification/notification_detail_page.dart
@@ -21,38 +21,42 @@ class NotificationDetailPage extends StatelessWidget {
           style: style,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Gap(24),
-            const Text(
-              '2023/2/23',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-              ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Gap(24),
+                const Text(
+                  '2023/2/23',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Gap(8),
+                const Text(
+                  '最新版アプリが公開されました。ぜひアップデートをお願いします。',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Gap(8),
+                Divider(
+                  color: Colors.grey[200],
+                ),
+                const Gap(8),
+                // お知らせ内容の仮文章を追加
+                const Text(
+                  'いつもTRIP TONICをご利用いただき誠にありがとうございます。運営のshigurenaです。\n\nTRIP TONICの最新版（2.0.0）をリリースしましたので、ぜひアップデートをお願いします！今回のアップデート内容は以下のとおりです。\n\n・タイムライン機能の追加\n→ユーザ同士で旅プランの共有や投稿ができるようになりました！頑張って作りましたので皆さんにぜひ使っていただきたいです！\n\n・軽微なバグの修正\n→ユーザの皆様からいただいた意見を元に軽微なバグの修正を行いました！\n\nまた、ご要望やバグ等のお問い合わせは下記からお願いいたします！\n\nhttps://google.com\n\n今後ともTRIP TONICをよろしくお願いいたします！',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
             ),
-            const Gap(8),
-            const Text(
-              '最新版アプリが公開されました。ぜひアップデートをお願いします。',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const Gap(8),
-            Divider(
-              color: Colors.grey[200],
-            ),
-            const Gap(8),
-            // お知らせ内容の仮文章を追加
-            const Text(
-              'いつもTRIP TONICをご利用いただき誠にありがとうございます。運営のshigurenaです。\n\nTRIP TONICの最新版（2.0.0）をリリースしましたので、ぜひアップデートをお願いします！今回のアップデート内容は以下のとおりです。\n\n・タイムライン機能の追加\n→ユーザ同士で旅プランの共有や投稿ができるようになりました！頑張って作りましたので皆さんにぜひ使っていただきたいです！\n\n・軽微なバグの修正\n→ユーザの皆様からいただいた意見を元に軽微なバグの修正を行いました！\n\nまた、ご要望やバグ等のお問い合わせは下記からお願いいたします！\n\nhttps://google.com\n\n今後ともTRIP TONICをよろしくお願いいたします！',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
